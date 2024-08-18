2df1a3ffd81cdb056ed3261f32915014a0033a83
JoowonJang <wndnjs5248@hanmail.net>
2024-08-18 16:26:17 +0900

    [4주차] refactor: TextInput 컴포넌트에 id prop 추가

--- 4주차/src/components/TextInput/TextInput.tsx
+++ 4주차/src/components/TextInput/TextInput.tsx
@@ -3,11 +3,13 @@ import "./TextInput.scss";
 interface TextInputPropsType {
   type: "text" | "textarea";
   placeholder: string;
+  id: string;
 }
 
-const TextInput = ({ type, placeholder }: TextInputPropsType) => {
-  if (type === "text") return <input type="text" placeholder={placeholder} className="text-input" />;
-  else if (type === "textarea") return <textarea placeholder={placeholder} rows={3} className="text-input textarea" />;
+const TextInput = ({ id, type, placeholder }: TextInputPropsType) => {
+  if (type === "text") return <input type="text" placeholder={placeholder} id={id} className="text-input" />;
+  else if (type === "textarea")
+    return <textarea placeholder={placeholder} rows={3} id={id} className="text-input textarea" />;
 };
 
 export default TextInput;
