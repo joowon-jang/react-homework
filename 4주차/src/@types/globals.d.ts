8de45812b64574bbd8ec16d225df21d7b648f04d
JoowonJang <wndnjs5248@hanmail.net>
2024-08-19 00:58:03 +0900

    [4주차] refactor: 전역 타입 수정
    - TodoListType 추가
    - DoItType에 done, archived 추가
    - StatusType 추가

--- 4주차/src/@types/globals.d.ts
+++ 4주차/src/@types/globals.d.ts
@@ -3,6 +3,19 @@ interface DoItType {
   description: string;
   startTime: Date;
   endTime: Date;
+  done: boolean;
+  archived: boolean;
+}
+
+interface TodoListType {
+  id: string;
+  title: string;
+  description: string;
+  startTime: string;
+  endTime: string;
+  done: boolean;
+  archived: boolean;
 }
 
 type NoonType = "오전" | "오후";
+type StatusType = "모두" | "할일" | "한일" | "보관";
