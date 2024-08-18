307a49108486133b5d61875caf6e605ae544729d
JoowonJang <wndnjs5248@hanmail.net>
2024-08-18 17:32:18 +0900

    [4주차] feat: 전체 앱 기본 스타일링

--- 4주차/src/App.tsx
+++ 4주차/src/App.tsx
@@ -1,20 +1,18 @@
+import { useEffect, useState } from "react";
 import "./App.scss";
-import Button from "./components/Button/Button";
-import Checkbox from "./components/Checkbox/Checkbox";
-import Archive from "./components/Archive/Archive";
-import Status from "./components/Status/Status";
 import Logo from "./components/Logo/Logo";
-import TextInput from "./components/TextInput/TextInput";
-import Time from "./components/Time/Time";
+import formatDate from "./utils/formatDate";
+import Button from "./components/Button/Button";
+import PlusImage from "/plus.svg";
+import Status from "./components/Status/Status";
 import DoIt from "./components/DoIt/DoIt";
-import Select from "./components/Select/Select";
+import Divider from "./components/Divider/Divider";
 import ModalDialog from "./components/ModalDialog/ModalDialog";
-import { useEffect, useId, useState } from "react";
 
 function App() {
-  const id1 = useId();
-  const id2 = useId();
-  const [isModalOpen, setIsModalOpen] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const dateOfToday = new Date();
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
@@ -31,45 +29,70 @@ function App() {
   }, [isModalOpen]);
 
   return (
-    <div id="app">
-      <Button modifier="primary">레이블</Button>
-      <Button modifier="secondary">레이블</Button>
-      <Button modifier="disabled">레이블</Button>
-      <Checkbox />
-      <Archive />
-      <Logo type="mono" />
-      <Logo type="stereo" />
-      <ul>
+    <div id="app" className="doit-app">
+      <Logo type="stereo" style={{ marginBottom: "44px" }} />
+
+      <h1 className="doit-app__title">우리, 오늘 뭐할까?</h1>
+      <time className="doit-app__date">{formatDate(dateOfToday)}</time>
+
+      <Button modifier="primary" onClick={openModal} style={{ marginBottom: "32px" }}>
+        <img src={PlusImage} alt="플러스" /> 생각났어?
+      </Button>
+
+      <ul className="doit-app-status-list">
         <li>
-          <Status count={3}>모두</Status>
+          <Status count={0} defaultChecked={true}>
+            모두
+          </Status>
+        </li>
+        <Divider type="vertical" style={{ height: "12px" }} />
+        <li>
+          <Status count={0}>할일</Status>
         </li>
         <li>
-          <Status count={0}>모두</Status>
+          <Status count={0}>한일</Status>
         </li>
         <li>
-          <Status count={1}>모두</Status>
-        </li>
-        <li>
-          <Status count={2}>모두</Status>
+          <Status count={0}>보관</Status>
         </li>
       </ul>
-      <TextInput type="text" placeholder="오늘 할 일 제목" id={id1} />
-      <TextInput
-        type="textarea"
-        placeholder="오늘 할 일 내용을 입력합니다. 할 일 내용은 가급적 간결하고 이해하기 쉽게 요약해서 작성합니다."
-        id={id2}
-      />
-      <Time />
-      <DoIt
-        content={{
-          title: "할 일",
-          description: "할 일 내용을 작성합니다.",
-          noon: "오전",
-          startTime: "07:30",
-          endTime: "09:00",
-        }}
-      />
-      <Select />
+
+      <ul className="doit-app-doit-list">
+        <li>
+          <DoIt
+            content={{
+              title: "동네 주민 모임",
+              description: "동네 주민 정기 모임일. 저녁 식사 후, 모임 회의 가질 예정.",
+              noon: "오후",
+              startTime: "07:00",
+              endTime: "09:30",
+            }}
+          />
+        </li>
+        <li>
+          <DoIt
+            content={{
+              title: "동네 주민 모임",
+              description: "동네 주민 정기 모임일. 저녁 식사 후, 모임 회의 가질 예정.",
+              noon: "오후",
+              startTime: "07:00",
+              endTime: "09:30",
+            }}
+          />
+        </li>
+        <li>
+          <DoIt
+            content={{
+              title: "동네 주민 모임",
+              description: "동네 주민 정기 모임일. 저녁 식사 후, 모임 회의 가질 예정.",
+              noon: "오후",
+              startTime: "07:00",
+              endTime: "09:30",
+            }}
+          />
+        </li>
+      </ul>
+
       <ModalDialog isOpen={isModalOpen} onClose={closeModal} />
     </div>
   );
